f3e4d6ebc3cab85e9c05c4584afc7b1312e1a7de
sktan18 <sktan18@gmail.com>
2022-07-23 11:53:09 +1200

    Add project files.

---
+++ BinaryTreeCalculator/BinaryTreeCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeCalculator
+{
+    public static class BinaryTreeCalculator
+    {
+        public static int EvaluateExpression(string expression)
+        {
+            return 0;
+        }
+    }
+}
---
+++ BinaryTreeUnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿
+namespace BinaryTreeUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase("(1 + 1) x 2", ExpectedResult=4)]
+        [TestCase("((15 ÷ (7 − (1 + 1) ) ) × -3 ) − (2 + (1 + 1))", ExpectedResult = -13)]
+        public int TestExpressions(string expression)
+        {
+
+            return BinaryTreeCalculator.BinaryTreeCalculator.EvaluateExpression(expression);
+        }
+    }
+}
